cbffd6ab7f916db39a444d8220887582dcd9e9f7
Aniq Ahsan <aniq.ahsan@eng.ox.ac.uk>
2022-06-29 17:05:22 +0100

    Simulation works. Changed main.py so that it now plots the size of assets in the network.

--- Code/Network/Network.py
+++ Code/Network/Network.py
@@ -179,7 +179,7 @@ class Network_STEVFNs:
         og_df["Asset_Size"] = asset_sizes_array
         max_asset_size = np.max(asset_sizes_array)
         # Set minimum asset size to plot
-        min_asset_size = max_asset_size * 1E-2
+        min_asset_size = max_asset_size * 1E-3
         # Remove all assets that are too small
         con1 = og_df["Asset_Size"] >= min_asset_size
         og_df = og_df[con1]
