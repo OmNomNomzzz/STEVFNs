508aad2c44a09c5ae1ccff2b86c2c37700f3e263
Aniq Ahsan <aniq_ahsan@hotmail.com>
2022-09-28 14:05:29 +0100

    Code used for DPhil plotting.

--- main.py
+++ main.py
@@ -48,8 +48,8 @@ end_time = time.time()
 print("Time taken to build network = ", end_time - start_time, "s")
 
 
-for counter1 in range(len(scenario_folders_list)):
-# for counter1 in range(1):
+# for counter1 in range(len(scenario_folders_list)):
+for counter1 in range(1):
     ### Read Input Files ###
     scenario_folder = scenario_folders_list[counter1]
     asset_parameters_filename = os.path.join(scenario_folder, "Asset_Parameters.csv")
@@ -81,11 +81,18 @@ for counter1 in range(len(scenario_folders_list)):
     ### Plot Results ############
     print("Time taken to solve problem = ", end_time - start_time, "s")
     print("Total cost to satisfy all demand = ", my_network.problem.value, " Billion USD")
-    DPhil_Plotting.plot_all(my_network)
-    # DPhil_Plotting.plot_asset_sizes(my_network)
-    # DPhil_Plotting.plot_asset_costs(my_network)
+    # DPhil_Plotting.plot_all(my_network)
+    DPhil_Plotting.plot_asset_sizes(my_network)
+    DPhil_Plotting.plot_asset_costs(my_network)
     
+##### Plot flows for BAU scenario ######
+DPhil_Plotting.plot_SG_EL_input_flows_BAU(my_network)
+DPhil_Plotting.plot_SG_EL_output_flows_BAU(my_network)
+DPhil_Plotting.plot_RE_EL_input_flows_BAU(my_network)
+DPhil_Plotting.plot_RE_EL_output_flows_BAU(my_network)
 
-
-
+DPhil_Plotting.plot_SG_NH3_input_flows_BAU(my_network)
+DPhil_Plotting.plot_SG_NH3_output_flows_BAU(my_network)
+DPhil_Plotting.plot_RE_NH3_input_flows_BAU(my_network)
+DPhil_Plotting.plot_RE_NH3_output_flows_BAU(my_network)
 
