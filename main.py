b3431b03039c7fe75e3eb78dd3a0d165288f410e
Monica Sagastuy <monica.sagastuybrena@eng.ox.ac.uk>
2022-12-06 13:29:51 +0000

    Updated RE asset brands for location specific costs in all case studies

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ from Code.Plotting import DPhil_Plotting
 
 #### Define Input Files ####
 # case_study_name = "Autarky_Case_Study"
-case_study_name = "Xlinks_Case_Study" 
-# case_study_name = "Xlinks_EXT_Case_Study"
+# case_study_name = "Xlinks_Case_Study" 
+case_study_name = "Xlinks_EXT_Case_Study"
 
 
 base_folder = os.path.dirname(__file__)
