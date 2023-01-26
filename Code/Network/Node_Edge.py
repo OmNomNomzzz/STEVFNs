ec6b68d39708251a6ce687eea2fa37552987454d
Aniq Ahsan <aniq_ahsan@hotmail.com>
2023-01-27 07:47:00 +0800

    made some changes to Node_Edge.py

--- Code/Network/Node_Edge.py
+++ Code/Network/Node_Edge.py
@@ -12,19 +12,21 @@ class Node:
     def __init__(self):
         self.input_edges = set()
         self.output_edges = set()
-        self.net_output_flows = cp.Constant(0)
         self.eq_cons = []#Equality constraints
         self.ieq_cons = []#Inequality constraints
-        self.net_output_flows_conserved = dict()
-        self.net_output_flows_curtailed = dict()
+        self.net_output_flows_conserved = cp.Constant(0)
+        self.net_output_flows_curtailed = cp.Constant(0)
+        self.conserved_flows_types = []
+        self.curtailed_flows_types = []
         return
 
 class Edge:
     def __init__(self):
         self.source_node = False
         self.target_node = False
-        self.x_flows = dict()
-        self.y_flows = dict()
+        self.flows_x = cp.Constant(0)
+        self.flows_types_x = []
+        self.flows_types_y = []
         return
 
 
