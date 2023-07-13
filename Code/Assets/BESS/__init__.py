ea7d2f0619e46de4b9a77887c8008731a44f960d
Monica Sagastuy <monica.sagastuybrena@eng.ox.ac.uk>
2023-07-13 11:56:45 +0100

    Minimum constant parameter name fixed

--- Code/Assets/BESS/__init__.py
+++ Code/Assets/BESS/__init__.py
@@ -137,14 +137,14 @@ class Storage_Asset(Asset_STEVFNs):
     def cost_fun(flows, params):
         sizing_constant = params["storage_sizing_constant"]
         usage_constant_1 = params["storage_usage_constant"]
-        # minimum_size = params["minimum_constant"]
+        # minimum_constant = params["minimum_constant"]
         
         
         return cp.maximum(sizing_constant * cp.max(flows),  usage_constant_1 * cp.sum(flows))
         # return sizing_constant * cp.max(flows)
         
         # Setting a minimum size for the assets, Supergrid case study sensitivity analysis; uncomment minimum size for parameters
-        # return cp.maximum(sizing_constant * cp.maximum(cp.max(flows), minimum_size),  usage_constant_1 * cp.sum(flows))
+        # return cp.maximum(sizing_constant * cp.maximum(cp.max(flows), minimum_constant),  usage_constant_1 * cp.sum(flows))
     
     @staticmethod
     def conversion_fun(flows, params):
