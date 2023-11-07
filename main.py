ead7144ed6439ccde02f0dd1e49089b569fee909
Monica Sagastuy <monica.sagastuybrena@eng.ox.ac.uk>
2023-11-07 19:42:59 +0000

    reset timesteps for SG-ID_aut

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from Code.Results import GMPA_Results
 ###### Autarky Case Studies #########
 # case_study_name = "Autarky_SG"
 # case_study_name = "Autarky_ID"
-case_study_name = "Autarky_MY"
+# case_study_name = "Autarky_MY"
 # case_study_name = "Autarky_VN"
 # case_study_name = "Autarky_PH"
 # case_study_name = "Autarky_TH"
@@ -32,7 +32,7 @@ case_study_name = "Autarky_MY"
 # case_study_name = "Autarky_LA"
 
 ###### Two Country Case Studies #########
-# case_study_name = "SG-ID_Autarky"
+case_study_name = "SG-ID_Autarky"
 # case_study_name = "SG-ID_Collab"
 
 # case_study_name = "SG-MY_Autarky"
