4c7baf110e31ebf676234afb2a78ca72a4e76ac9
Mónica Sagastuy Breña <monica.sagastuybrena@eng.ox.ac.uk>
2024-09-27 16:28:19 +0100

    Updated create_RE_potentials.py and added automation to process res_analysis data

--- create_RE_potentials_files.py
+++ create_RE_potentials_files.py
@@ -31,11 +31,13 @@ onshore_wind_dir = os.path.join(root_dir, "Code", "Assets", "RE_WIND_Onshore_Lim
 offshore_wind_dir = os.path.join(root_dir, "Code", "Assets", "RE_WIND_Offshore_Lim", "profiles")
 rooftop_pv_dir = os.path.join(root_dir, "Code", "Assets", "RE_PV_Rooftop_Lim", "profiles")
 openfield_pv_dir = os.path.join(root_dir, "Code", "Assets", "RE_PV_Openfield_Lim", "profiles")
+openfield_pvbau_dir = os.path.join(root_dir, "Code", "Assets", "RE_PV_Openfield_BAU", "profiles")
+onshore_windbau_dir = os.path.join(root_dir, "Code", "Assets", "RE_WIND_Onshore_BAU", "profiles")
 
 loc_params_df = pd.read_csv(loc_params_file)
 loc_params_df.set_index(loc_params_df['location_name'], inplace=True)
 
-countries = ['MA', 'DZ', 'TN', 'LY', 'EG', 'NG', 'BR', 'CO', 'PE', 'CL']
+countries = ['MA', 'KE', 'ZA', 'KR', 'EG', 'NG', 'BR', 'CO', 'PE', 'CL']
 
 final_lat_lon_df = pd.DataFrame()
 
@@ -56,6 +58,14 @@ for country in countries:
         os.makedirs(wind_on_directory)
     wind_on_filename = os.path.join(wind_on_directory, f'WINDOUT_lat{lat}_lon{lon}.csv')
     with open(wind_on_filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        
+    # Create empty RE file for onshore wind  in BAU asset
+    wind_onbau_directory = os.path.join(onshore_windbau_dir, 'WINDOUT', f'lat{lat}')
+    if not os.path.exists(wind_onbau_directory):
+        os.makedirs(wind_onbau_directory)
+    wind_onbau_filename = os.path.join(wind_onbau_directory, f'WINDOUT_lat{lat}_lon{lon}.csv')
+    with open(wind_onbau_filename, mode='w', newline='') as file:
         writer = csv.writer(file)
        
     # Create empty RE file for offshore wind
@@ -82,11 +92,24 @@ for country in countries:
     with open(pv_of_filename, mode='w', newline='') as file:
         writer = csv.writer(file)
     
-    # Create a csv file to view each country's name and rounded lat lon values
-    final_lat_lon_df = pd.concat([final_lat_lon_df,
-                                  pd.DataFrame({'location_name': [country], 'lat': [lat],
-                                                'lon': [lon]})], ignore_index=True)
-    final_lat_lon_df.to_csv(os.path.join(root_dir, "Location_Parameters_for_RE_Profiles.csv"), index=False)
+    # Create empty RE file for openfield pv in BAU asset
+    pv_ofbau_directory = os.path.join(openfield_pvbau_dir, 'PVOUT', f'lat{lat}')
+    if not os.path.exists(pv_ofbau_directory):
+        os.makedirs(pv_ofbau_directory) 
+    pv_ofbau_filename = os.path.join(pv_ofbau_directory, f'PVOUT_lat{lat}_lon{lon}.csv')
+    with open(pv_ofbau_filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        
+        
+    
+    
+    # Create a csv file to view each country's name and rounded lat lon values, if useful
+    
+    
+    # final_lat_lon_df = pd.concat([final_lat_lon_df,
+    #                               pd.DataFrame({'location_name': [country], 'lat': [lat],
+    #                                             'lon': [lon]})], ignore_index=True)
+    # final_lat_lon_df.to_csv(os.path.join(root_dir, "Location_Parameters_for_RE_Profiles.csv"), index=False)
     
 
     
