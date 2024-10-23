4dbe053782a7c05b0048d6b559ba291f23deb631
Mónica Sagastuy Breña <monica.sagastuybrena@eng.ox.ac.uk>
2024-10-23 14:23:15 +0100

    First test run of Least_Cost_Emissions, needs to be rerun with updated costs for all assets

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from Code.Results import GMPA_Results
 
 
 #### Define Input Files ####
-case_study_name = "BAU_No_Action"
+case_study_name = "Least_Cost_Emissions"
 
 
 base_folder = os.path.dirname(__file__)
