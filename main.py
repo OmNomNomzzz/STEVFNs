bc278831989c62c7886b9a5c4927746a47a44680
Lemomons <aimans333@gmail.com>
2025-02-22 21:32:16 +0800

    updated total data and mitigation curve

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from Code.Results import GMPA_Results
 #### Define Input Files ####
 # case_study_name = "BAU_No_Action"
 # case_study_name = "Autarky_CL"
-case_study_name = "VN-MY-LA-SG_Collab"
+case_study_name = "BN-TH-PH-MY_Collab"
 # case_study_name = "KH-BN_Collab"
 # case_study_name = "TH-PH-MY_Autarky"
 # case_study_name = "TH-PH-MY_Collab"
